755f8d818b53d4b0b4ab6ddee9b28c55ed2d5bbf
NJahromi <nathan.jahromi@gmail.com>
2018-11-16 21:22:17 -0500

    Cleaned up my extremely messy repository

--- JavaCars/src/Vehicle.java
+++
@@ -1,7 +0,0 @@
-public class Vehicle {
-
-	public void move() {
-		System.out.println("Plane is flying");
-	}
-
-}
--- JavaPlanes/src/Vehicle.java
+++
@@ -1,7 +0,0 @@
-public class Vehicle {
-
-	public void move() {
-		System.out.println("Vehicle is moving");
-	}
-
-}
